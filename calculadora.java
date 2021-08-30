ca901ec2f7d1d4d0679c9ee5d28c5e1ef5e5139d
Renan Inacio Becker <51799627+RenanBecker@users.noreply.github.com>
2021-08-30 20:22:38 +0000

    -Força o usuário a escolher uma das operações disponíveis -Limpa a tela

--- calculadora.java
+++ calculadora.java
@@ -1,44 +1,53 @@
 import java.util.Scanner;
+
 public class calculadora {
-	public static void main(String[] args){
-		int primeiro;
-		int segundo;
-		int resultado;
-		String operacao;
+  public static void main(String[] args){
+    int primeiro;
+    int segundo;
+    int resultado;
     char novamente = 'a';
-    
+    char operacao = 'a';
+
     System.out.println("Bem vindo ao programa Calculadora");
-    do{
-        Scanner ler = new Scanner(System.in);
-        System.out.println("Digite o primeiro número da operação");
-        primeiro = ler.nextInt();
-        System.out.println("Escolha a operação que deseja realizar\n+ para somar\n- para subtrair\n* para multiplicar\n/ para dividir");
-        operacao = ler.next();
-        System.out.println("Digite o segundo número da operação");
-        segundo = ler.nextInt();
-        switch (operacao) {
-          case "+":
-            resultado = primeiro + segundo;
-            System.out.println(primeiro + " + " + segundo +  " = " + resultado);
-            break;
-          case "-":
-            resultado = primeiro - segundo;
-            System.out.println(primeiro + " - " + segundo +  " = " + resultado);
-            break;
-          case "*":
-            resultado = primeiro * segundo;
-            System.out.println(primeiro + " * " + segundo +  " = " + resultado);
-            break;
-          case "/":
-            resultado = primeiro / segundo;
-            System.out.println(primeiro + " / " + segundo +  " = " + resultado);
-            break;
-          default:
-            System.out.println("Operação aritimética inválida");
+    do {
+      Scanner ler = new Scanner(System.in);
+      System.out.println("Digite o primeiro número da operação");
+      primeiro = ler.nextInt();
+      while((operacao != '+') && (operacao != '-') && (operacao != '*') && (operacao != '/'))
+      {
+        System.out.println(
+            "Escolha a operação que deseja realizar\n+ para somar\n- para subtrair\n* para multiplicar\n/ para dividir");
+        operacao = ler.next().charAt(0);
+          if((operacao != '+') && (operacao != '-') && (operacao != '*') && (operacao != '/')){
+            System.out.print("\033[H\033[2J"); 
+            System.out.println("Escolha invalida");
+          }
         }
-        System.out.println("Deseja realizar outro calculo?\n s = sim\n n = não");
-        novamente = ler.next().charAt(0);
-      }while(novamente == 's');
+      System.out.println("Digite o segundo número da operação");
+      segundo = ler.nextInt();
+      switch (operacao) {
+        case '+':
+          resultado = primeiro + segundo;
+          System.out.println(primeiro + " + " + segundo + " = " + resultado);
+          break;
+        case '-':
+          resultado = primeiro - segundo;
+          System.out.println(primeiro + " - " + segundo + " = " + resultado);
+          break;
+        case '*':
+          resultado = primeiro * segundo;
+          System.out.println(primeiro + " * " + segundo + " = " + resultado);
+          break;
+        case '/':
+          resultado = primeiro / segundo;
+          System.out.println(primeiro + " / " + segundo + " = " + resultado);
+          break;
+      }
+      System.out.println("Deseja realizar outro calculo?\n s = sim\n n = não");
+      novamente = ler.next().charAt(0);
+      operacao = 'a';
+      System.out.print("\033[H\033[2J"); 
+    } while (novamente == 's');
     System.out.println("Obrigado volte sempre");
-	}
+  }
 }
