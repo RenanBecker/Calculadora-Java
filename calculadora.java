4c01ac4656d8553c5066e9a26759646bd1ad76f5
Renan Becker <51799627+RenanBecker@users.noreply.github.com>
2021-09-16 13:21:20 +0000

    Adicionado equação do segundo grau

--- calculadora.java
+++ calculadora.java
@@ -5,7 +5,7 @@ public class calculadora {
 
 	static int menu() {
 		menus();
-		int op = checa();
+		int op = (int)checa();
 		return op;
 	}
 
@@ -18,12 +18,13 @@ public class calculadora {
 		System.out.println("(4) Divisão");
 		System.out.println("(5) Raiz Quadrada");
 		System.out.println("(6) Potência");
+		System.out.println("(7) Equação do Segundo Grau");
 		System.out.println("(0) Sair");
 		System.out.print("\nOpção: ");
 	}
 	
-	static int checa() {
-		int numero;
+	static double checa() {
+		double numero;
 		Scanner ler = new Scanner(System.in);
 		while (true) {
 			try {
@@ -31,7 +32,6 @@ public class calculadora {
 				break;
 			} catch (Exception e) {
 				System.out.print("Por favor digite apenas números\n");
-				menus();
 				ler.nextLine();
 			}
 		}
@@ -62,44 +62,63 @@ public class calculadora {
 		return Math.pow(n1, n2);
 	}
 
+	static void segundoGrau() {
+		Scanner ler = new Scanner(System.in);
+		double a, b, c, xMais, xMenos, delta;
+
+		System.out.println("Bem vindo a calculadora de segundo grau");
+		System.out.println("Informe as variaveis da fórmula ax2+bx+c=0");
+		System.out.println("Informe o valor de A");
+		a = checa();
+
+		if (a == 0) { // testa o valor de A, caso seja zero ele encerra
+			System.out.println("O valor de A é zero, então não é equação de segundo grau");
+			System.out.println("A calculadora sera encerrada");
+		} else { // executa o programa caso A != 0
+			System.out.println("Informe o valor de B");
+			b = checa();
+			System.out.println("Informe o valor de C");
+			c = checa();
+
+			delta = b * b - ((4 * a) * c); //resolve delta
+			xMais = (b * (-1)) + Math.sqrt(delta); 
+			xMenos = (b * (-1)) - Math.sqrt(delta);
+			if (delta < 0) { //verifica se possui valores de X
+				System.out.println("O delta é negativo\nA calculadora de segundo grau sera encerrada");
+			} else if (delta == 0) { //verifica se possui somente 1 valor de X
+				System.out.println("O delta vale zero então a equação possui apenas um valor de X");
+				System.out.println("O valor de X é: " + xMais);
+			} else { // equação com dois valores de X
+				System.out.println("A equação possui duas raizes reais");
+				System.out.println("Os valores são: X' = " + xMais + " X\"= " + xMenos);
+			}
+
+		}
+
+	}
+	
 	public static void main(String[] args) {
 		Scanner leitor = new Scanner(System.in);
 		int opcao;
 		double n1 = 0, n2 = 0, resultado = 0;
+		opcao = menu();
+		
+		switch (opcao) {
+		case 7:
+			segundoGrau();
+			break;
+		}
+		
 		do {
 			opcao = menu();
 			if (opcao >= 1 && opcao <= 4 || opcao == 6) {
-				while (true) {
 					System.out.println("Digite o primeiro número da operação");
-					try {
-						n1 = leitor.nextDouble();
-						break;
-					} catch (Exception e) {
-						System.out.print("Por favor digite apenas números\n");
-						leitor.nextLine();
-					}
-				}
-				while (true) {
+					n1 = checa();
 					System.out.println("Digite o segundo número da operação");
-					try {
-						n2 = leitor.nextDouble();
-						break;
-					} catch (Exception e) {
-						System.out.print("Por favor digite apenas números\n");
-						leitor.nextLine();
-					}
-				}
+					n2 = checa();
 			} else if (opcao == 5) {
-				while (true) {
 					System.out.println("Digite o número da operação");
-					try {
-						n1 = leitor.nextDouble();
-						break;
-					} catch (Exception e) {
-						System.out.print("Por favor digite apenas números\n");
-						leitor.nextLine();
-					}
-				}
+					n1 = checa();
 			}
 			boolean apresentaResultado = true;
 			switch (opcao) {
@@ -108,12 +127,15 @@ public class calculadora {
 				break;
 			case 1:
 				resultado = soma(n1, n2);
+				System.out.println("Soma");
 				break;
 			case 2:
 				resultado = multiplicacao(n1, n2);
+				System.out.println("Multiplicação");
 				break;
 			case 3:
 				resultado = subtracao(n1, n2);
+				System.out.println("Subtração");
 				break;
 			case 4:
 				resultado = divisao(n1, n2);
@@ -121,12 +143,15 @@ public class calculadora {
 					System.out.println("Valor Inválido para o Segundo Número!");
 					apresentaResultado = false;
 				}
+				System.out.println("Divisão");
 				break;
 			case 5:
 				resultado = raizQuadrada(n1);
+				System.out.println("Raiz Quadrada");
 				break;
 			case 6:
 				resultado = potencia(n1, n2);
+				System.out.println("Potenciação");
 				break;
 			default:
 				apresentaResultado = false;
